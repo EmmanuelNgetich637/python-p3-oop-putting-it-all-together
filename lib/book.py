afd21f465944dfc763969a15a4e6a39d168eb6c2
Emmanuel Ngetich <engetich333@gmail.com>
2025-05-10 07:34:44 -0700

    Add and pass all tests for Book and Shoe classes

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python3
 
-class Book:
-    pass
+class Book:# Define a class named Book
+    def __init__(self, title, page_count):#(__init__: a special method that runs when you create an of the class.)
+        self.title = title # Save the book title to the instance 
+        self._page_count = None # initialize private variable for page_count
+        self.page_count = page_count # use the setter to validate and set page_count 
+        #(self.title and self.page_count stores the values pased in when creating the Book object)
+    @property
+    def page_count(self): #Getter method: used to access the page_count
+        return self._page_count # Return the value of the private variable
     
+    @page_count.setter
+    def page_count(self, value):#Setter metod: used to validate and set page_count 
+        if isinstance(value, int): # Check if the value is an integer
+            self._page_count = value #If valid, set the private variable
+        else:
+            print("page_count must be an integer") # Print error if value is not an integer
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!") # Print message when method is called
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand # set brand directly
+        self._size = None # private variable for size
+        self.size = size # use setter to validate
+        self.condition = None # default condition is None
+
+    @property
+    def size(self):
+        return self._size #return private size
+    
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int): # check if size is int
+            self._size = value 
+        else:
+            print("size must be an integer") # error message if invalid
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New" # set condition to 'New'
